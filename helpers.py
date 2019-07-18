d9af03fda035ac611121ec5ea5d3a358bad60cd7
yandenghong <vincentyim8@163.com>
2019-07-18 17:13:42 +0800

    Added communicate for interprocess communication

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import socket
+
+
+def communicate(dispatcher_host, dispatcher_port, command):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((dispatcher_host, dispatcher_port))
+    s.send(command)
+    s.close()
+
+
