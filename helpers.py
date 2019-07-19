1ab914eda1a9b66240f9ea1124a7052fe1568503
yandenghong <vincentyim8@163.com>
2019-07-19 11:14:01 +0800

    Added exception catch

--- helpers.py
+++ helpers.py
@@ -3,7 +3,10 @@ import socket
 
 def communicate(dispatcher_host, dispatcher_port, command):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((dispatcher_host, dispatcher_port))
+    try:
+        s.connect((dispatcher_host, dispatcher_port))
+    except (ConnectionRefusedError, ConnectionResetError):
+        return
     s.sendall(command.encode())
     response = s.recv(1024)
     s.close()
