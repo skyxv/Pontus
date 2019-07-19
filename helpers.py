3217df0dce728235fc6354f141fa5f316a38cbdc
yandenghong <vincentyim8@163.com>
2019-07-19 09:25:35 +0800

    Added return response

--- helpers.py
+++ helpers.py
@@ -4,7 +4,9 @@ import socket
 def communicate(dispatcher_host, dispatcher_port, command):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((dispatcher_host, dispatcher_port))
-    s.send(command.encode())
+    s.sendall(command.encode())
+    response = s.recv(1024)
     s.close()
+    return response
 
 
