f48154cda218327ca967ce11594ad453623c8d05
yandenghong <vincentyim8@163.com>
2019-07-19 09:16:53 +0800

    Updated send bytes

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ import socket
 def communicate(dispatcher_host, dispatcher_port, command):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((dispatcher_host, dispatcher_port))
-    s.send(command)
+    s.send(command.encode())
     s.close()
 
 
